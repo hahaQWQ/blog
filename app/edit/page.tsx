28ccfcc96e82c0ad8c3d2dee40c4e6fb23c348c6
haha <3109944373@qq.com>
2024-06-12 09:04:01 +0800

    init

---
+++ app/edit/page.tsx
@@ -0,0 +1,12 @@
+
+"use client"
+import "@blocknote/core/fonts/inter.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+ 
+
+export default function Edit() {
+    const editor = useCreateBlockNote();
+    return <BlockNoteView editor={editor} />;
+};
---
+++ app/post/page.tsx
@@ -0,0 +1,3 @@
+export default function Post() {
+    return <>文章页面</>
+};
